bad614710f401133b0f6ee892a0a5b708d30820a
hieng55 <bosshieng55@gmail.com>
2024-08-23 12:43:47 +0700

    push code

--- js/index.js
+++ js/index.js
@@ -15,3 +15,51 @@ function handleClickOutsideContent(event) {
     bgBlack.classList.remove("active");
   }
 }
+function validateForm() {
+  let isValid = true;
+
+  // Xóa các thông báo lỗi trước đó
+  document.getElementById("nameError").textContent = "";
+  document.getElementById("emailError").textContent = "";
+  document.getElementById("phoneError").textContent = "";
+
+  // Kiểm tra tên
+  let name = document.getElementById("name").value;
+  if (name === "") {
+    document.getElementById("nameError").textContent = "Họ và tên không được bỏ trống";
+    isValid = false;
+  }
+
+  // Kiểm tra email
+  let email = document.getElementById("email").value;
+  if (email === "") {
+    document.getElementById("emailError").textContent = "Email không được bỏ trống";
+    isValid = false;
+  }
+
+  // Kiểm tra số điện thoại
+  let phone = document.getElementById("phone").value;
+  if (phone === "") {
+    document.getElementById("phoneError").textContent = "Số điện thoại không được bỏ trống";
+    isValid = false;
+  }
+
+  // Nếu tất cả các trường đều hợp lệ, tiến hành gửi email
+  if (isValid) {
+    sendEmail();
+  }
+}
+
+function sendMail() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+
+  let subject = `Liên hệ từ khách hàng ${name} `;
+  let body = "Họ và tên: " + name + " - " + "Email: " + email + " - " + "Số điện thoại: " + phone + " - " + "Lời nhắn: " + message;
+
+  let mailtoLink = "mailto:info@gmail.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+  window.location.href = mailtoLink;
+}
